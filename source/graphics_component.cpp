f103e6990a570900fe90664b37c23367af7319c1
Epiphane <exyphnos@gmail.com>
2015-04-12 16:21:23 -0700

    Test - adding new file

---
+++ source/graphics_component.cpp
@@ -0,0 +1,9 @@
+//
+//  graphics_component.cpp
+//  Project
+//
+//  Created by Thomas Steinke on 4/12/15.
+//
+//
+
+#include "graphics_component.h"
---
+++ source/graphics_component.h
@@ -0,0 +1,14 @@
+//
+//  graphics_component.h
+//  Project
+//
+//  Created by Thomas Steinke on 4/12/15.
+//
+//
+
+#ifndef __Project__graphics_component__
+#define __Project__graphics_component__
+
+#include <stdio.h>
+
+#endif /* defined(__Project__graphics_component__) */
