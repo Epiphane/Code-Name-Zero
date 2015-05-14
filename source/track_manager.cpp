b2080dcdc26c92430fc8f2e8d30367efec9e85f4
Thomas Steinke <exyphnos@gmail.com>
2015-05-14 16:10:24 -0700

    Hotfix track OBJ

--- source/track_manager.cpp
+++ source/track_manager.cpp
@@ -24,7 +24,7 @@ TrackManager::TrackManager(State *world, GameObject *player_in) {
    // NOTE:
    // Current track is 27.5 units long
    for (int i = 0; i<VISIBLE_TRACKS; i++) {
-      GameObject *track = new GameObject(ModelRenderer::load("models/Track/RGB_TrackOnly_Curved.obj", "models/Track/"));
+      GameObject *track = new GameObject(ModelRenderer::load("models/Track/track.obj", "models/Track/"));
       track->transform(nextRotate(next_track_number) * glm::scale(1.0f, 1.0f, TRACK_SCALE));
       track->setPosition(nextPosition(next_track_number));
       world->addObject(track);
@@ -41,7 +41,7 @@ void TrackManager::update(float dt, glm::vec3 player_position, State *world) {
    // If entering the next track segment
    if (movement->getLongPos() >= 0.5f || first) {
       first = false;
-      GameObject *track = new GameObject(ModelRenderer::load("models/Track/RGB_TrackOnly_Curved.obj", "models/Track/"));
+      GameObject *track = new GameObject(ModelRenderer::load("models/Track/track.obj", "models/Track/"));
       track->transform(nextRotate(next_track_number) * glm::scale(1.0f, 1.0f, TRACK_SCALE));
       track->setPosition(nextPosition(next_track_number));
       world->addObject(track);
