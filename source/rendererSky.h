c551b8f5e6c7b9eb1ddb17b942558ec946c99896
mlinsenbard <mlinsenb@calpoly.edu>
2015-05-25 19:51:05 -0700

    edited sources.txt

--- source/rendererSky.h
+++ source/rendererSky.h
@@ -10,5 +10,42 @@
 #define __RGBZero__rendererSky__
 
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "tiny_obj_loader.h"
+#include "renderer.h"
+#include "GLSL.h"
+
+class SkyRenderer : public Renderer {
+private:
+   GLuint v_buffer;
+   GLuint elements, texture;
+   
+   float z;
+   
+   static bool initialized;
+   void init();
+   static GLuint program;
+   static GLuint aPosition;
+   static GLuint uInvProjMatrix, uInvViewMatrix, uLightDir, uViewport, uFramebufferResolution;
+   
+public:
+   SkyRenderer(std::string texture, float z = 0);
+   
+   // Refers to the number of indices
+   void setNumElements(GLuint num) { elements = num; }
+   
+   // Send data to the GPU
+   void bufferData(DataType type, size_t size, void *data);
+   void bufferData(DataType type, const std::vector<float>& data);
+   void bufferData(DataType type, const std::vector<glm::vec2>& data);
+   
+   // Render the data!
+   void render(glm::mat4 Model);
+};
+
 
 #endif /* defined(__RGBZero__rendererSky__) */
