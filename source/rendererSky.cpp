51be6b824f4fe300457a5e86a4d6aec2a63fc2d9
mlinsenbard <mlinsenb@calpoly.edu>
2015-05-25 19:49:49 -0700

    initial commit - adding files

---
+++ source/rendererSky.cpp
@@ -0,0 +1,9 @@
+//
+//  rendererSky.cpp
+//  RGBZero
+//
+//  Created by Max Linsenbard on 5/25/15.
+//
+//
+
+#include "rendererSky.h"
---
+++ source/rendererSky.h
@@ -0,0 +1,14 @@
+//
+//  rendererSky.h
+//  RGBZero
+//
+//  Created by Max Linsenbard on 5/25/15.
+//
+//
+
+#ifndef __RGBZero__rendererSky__
+#define __RGBZero__rendererSky__
+
+#include <stdio.h>
+
+#endif /* defined(__RGBZero__rendererSky__) */
