58e5ad0cea9aac1bcb9f2a75b0fc3ada6a709368
David Ellison <davidellison341@gmail.com>
2015-06-11 12:00:44 -0700

    Difficulty stars

--- source/song_select_state.cpp
+++ source/song_select_state.cpp
@@ -21,24 +21,32 @@
 
 //sort of a constant
 #define HIGHEST_BPM 200
+#define STAR_OFFSET 0.07f
 
 //input callbacks--need ship here because
 //the functions need to be static
+bool done;
 int shipIndex;
 int currentSong = 0;
 LevelInfo *currentLevel;
 int MAX_SONGS;
 
+double timeDoneSet;
+
+const glm::vec2 RATING_START_POS = glm::vec2(0.0f, 0.0f);
+
 //todo move to class declaration
 ShipManager *shipManager;
 shared_ptr<GameObject> currentShip;
 
-void nextState();
+void triggerShipLeave();
 void scrollDown();
 void scrollUp();
 
 SongSelect::SongSelect(int shipndx) : StaticState("level_select_screen", audio_load_music("./audio/RGBZeroMenu.mp3", 135, true)) {
    shipIndex = shipndx;
+   
+   done = false;
    
    helper = RendererText::instance();
    helper->clearAllText();
@@ -51,7 +59,7 @@ SongSelect::SongSelect(int shipndx) : StaticState("level_select_screen", audio_l
 }
 
 void SongSelect::start() {
-   input_set_callback(GLFW_KEY_SPACE, nextState);
+   input_set_callback(GLFW_KEY_SPACE, triggerShipLeave);
    input_set_callback(GLFW_KEY_A, scrollUp);
    input_set_callback(GLFW_KEY_D, scrollDown);
    
@@ -94,16 +102,37 @@ void SongSelect::initializeList() {
 void SongSelect::render(float dt) {
    this->StaticState::render(dt);
    helper->render();
-   //ratingsRenderer->render();
+   ratingsRenderer->render();
 }
 
 void SongSelect::update(float dt) {
-   static float currentShipFloatAmt = 0.0f;
-   static bool floatingUp = true;
    
    helper->clearAllText();
    ratingsRenderer->clearAllRatings();
    
+   currentLevel = allSongs[currentSong];
+   
+   updateShipModel(dt);
+   updateRatings();
+   
+   helper->addText(glm::vec2(0.0, 0.3), allSongs[currentSong]->songTitle, glm::vec2(0.075), 1.0f);
+   helper->addText(glm::vec2(-0.2, 0.05), "Difficulty:", glm::vec2(0.07), 1.0f);
+   
+   helper->updateBuffers();
+   ratingsRenderer->updateBuffers();
+   
+}
+
+void SongSelect::updateRatings() {
+   //not very mathematical at all
+   ratingsRenderer->addRating(RATING_START_POS, currentLevel->bpm > 120 ? Full : Empty, glm::vec2(0.07f));
+   ratingsRenderer->addRating(RATING_START_POS + glm::vec2(STAR_OFFSET, 0.0), currentLevel->bpm > 140 ? Full : Empty, glm::vec2(0.07f));
+   ratingsRenderer->addRating(RATING_START_POS + glm::vec2(2 * STAR_OFFSET, 0.0), currentLevel->bpm > 180 ? Full : Empty, glm::vec2(0.07f));
+}
+
+void SongSelect::updateShipModel(float dt) {
+   static float currentShipFloatAmt = 0.0f;
+   static bool floatingUp = true;
    
    currentShip->clearModel();
    
@@ -119,17 +148,25 @@ void SongSelect::update(float dt) {
       currentShipFloatAmt -= 0.2 * dt;
    }
    
-   currentShip->transform(glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, -0.5f + currentShipFloatAmt, -1.0f)));
+   currentShip->transform(glm::rotate(glm::mat4(1.0f), 90.0f, glm::vec3(0.0f, 1.0f, 0.0f)));
    
-   helper->addText(glm::vec2(0.0, 0.3), allSongs[currentSong]->songTitle, glm::vec2(0.075), 1.0f);
-   
-   helper->updateBuffers();
-   ratingsRenderer->updateBuffers();
-   
-   currentLevel = allSongs[currentSong];
+   if (done) {
+      double time = glfwGetTime();
+      currentShip->transform(glm::translate(glm::mat4(1.0f), glm::vec3(-35.0f * (time - timeDoneSet), -0.5f + currentShipFloatAmt, -1.0f)));
+      if (time >= timeDoneSet + 0.2) {
+         toNextState();
+      }
+   } else {
+      currentShip->transform(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.5f + currentShipFloatAmt, -1.0f)));
+   }
 }
 
-void nextState() {
+void triggerShipLeave() {
+   done = true;
+   timeDoneSet = glfwGetTime();
+}
+
+void SongSelect::toNextState() {
    setState(new LoadingScreen(shipIndex, *currentLevel));
 }
 
